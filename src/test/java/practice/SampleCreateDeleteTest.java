55024e38fdb175024b0797531e35b33f68a1e524
sheno <sheno@HVSL>
2022-07-17 22:25:35 +0530

    this is first conflict meeting we are trying

--- src/test/java/practice/SampleCreateDeleteTest.java
+++ src/test/java/practice/SampleCreateDeleteTest.java
@@ -10,11 +10,15 @@ public class SampleCreateDeleteTest
 	public void createContact() throws Throwable
 	{
 		System.out.println("contact created");
+		System.out.println("create and delete");
+		System.out.println("vgcghfjh");
 		
 	}
 	@Test(groups = "regression")
 	public void createAndDeleteContact()throws Throwable
 	{
 		System.out.println("crated with delete options..");
+		System.out.println("delete");
 	}
 }
+
